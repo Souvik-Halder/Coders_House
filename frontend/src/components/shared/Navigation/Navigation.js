48a3b7d9853b8c98fd7115ce8fee02045a20447a
Souvik-Halder <souvik.halder2020@gift.edu.in>
2023-04-28 11:02:22 +0530

    Part 7 done tested

--- frontend/src/components/shared/Navigation/Navigation.js
+++ frontend/src/components/shared/Navigation/Navigation.js
@@ -20,7 +20,7 @@ const Navigation = () => {
     marginLeft:'10px'
   }
 
-  const {isAuth}=useSelector(state=>state.auth)
+  const {isAuth,user}=useSelector(state=>state.auth)
 
   async function logoutUser(){
    try {
@@ -32,13 +32,27 @@ const Navigation = () => {
    }
   }
   return (
- <nav className={`${styles.navbar} container`}>
-  <Link style={brandStyle} to={"/"}>
-   <img src='/images/logo.png' alt='Logo'/>
-   <span style={logoText}>Codershouse</span>
-  </Link>
-  {isAuth && <button onClick={logoutUser}>Logout</button>}
- </nav>
+    <nav className={`${styles.navbar} container`}>
+            <Link style={brandStyle} to="/">
+                <img src="/images/logo.png" alt="logo" />
+                <span style={logoText}>Codershouse</span>
+            </Link>
+         { isAuth &&   <div className={styles.navRight}>
+                <h3>{user.name}</h3>
+                <Link to="/">
+                    <img
+                        className={styles.avatar}
+                        src={user.avatar}
+                        width="40"
+                        height="40"
+                        alt="avatar"
+                    />
+                </Link>
+                <button className={styles.logoutButton} onClick={logoutUser}>
+                    <img src="/images/logout.png" alt="logout" />
+                </button>
+            </div>}
+        </nav>
   )
 }
 
--- frontend/src/pages/Steps/StepAvatar/StepAvatar.js
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.js
@@ -1,39 +1,54 @@
 
-import React,{useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../../../components/shared/Card/Card'
 import Button from '../../../components/shared/Button/Button'
 import styles from './StepAvatar.module.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { setAvatar } from '../../../store/activateSlice'
-import {activate} from '../../../http/index'
-import {setAuth} from '../../../store/authSlice'
+import { activate } from '../../../http/index'
+import { setAuth } from '../../../store/authSlice'
 import Loader from '../../../components/shared/Loader/Loader'
 
-function StepAvatar({onNext}) {
+function StepAvatar({ onNext }) {
 
- const {name,avatar}=useSelector(state=>state.activate);
- const dispatch=useDispatch();
-const [image, setImage] = useState('/images/monkey-avatar.png');
+  const { name, avatar } = useSelector(state => state.activate);
+  const dispatch = useDispatch();
+  const [image, setImage] = useState('/images/monkey-avatar.png');
   const [loading, setLoading] = useState(false);
-  async function submit(){
-    if(!name || !avatar) return;
-   try {
-    setLoading(true)
-    const {data}=await activate({name,avatar})
-   if(data.auth){
-    dispatch(setAuth(data));
-   }
-  } catch (error) {
-    console.log(error.message)
-   }finally{
-    setLoading(false);
-   }
+  const [unmounted, setUnmounted] = useState(false);
+
+
+  async function submit() {
+    if (!name || !avatar) return;
+    try {
+      setLoading(true)
+      const { data } = await activate({ name, avatar })
+      if (data.auth) {
+        //check
+        if (!unmounted) {
+          dispatch(setAuth(data));
+
+        }
+      }
+    } catch (error) {
+      console.log(error.message)
+    } finally {
+      setLoading(false);
+    }
   }
-  function captureImage(e){
-    const file=e.target.files[0];
-    const reader=new FileReader();
+
+  useEffect(() => {
+    //if we use any event listener we need to remove it
+    return () => {
+      setUnmounted(true);
+    }
+  })
+
+  function captureImage(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload=function(){
+    reader.onload = function () {
       setImage(reader.result)//fetching the base 64 url
 
       dispatch(setAvatar(reader.result))//after setting the base 64 url string it automatically sets the photo to display
@@ -41,35 +56,35 @@ const [image, setImage] = useState('/images/monkey-avatar.png');
     console.log(e)
   }
   return (
-     loading? <Loader message="Activation in progress...."/>:(
+    loading ? <Loader message="Activation in progress...." /> : (
       <>
-          <Card title={`Okay, ${name}`} icon="monkey-emoji">
-                <p className={styles.subHeading}>How’s this photo?</p>
-                <div className={styles.avatarWrapper}>
-                    <img
-                         className={styles.avatarImage}
-                        alt="avatar"
-                        src={image}
-                    />
-                </div>
-                <div>
-                    <input
-                    onChange={captureImage}
-                       className={styles.avatarInput}
-                        id="avatarInput"
-                        type="file"
-                      
-                    />
-                    <label className={styles.avatarLabel} htmlFor="avatarInput">
-                        Choose a different photo
-                    </label>
-                </div>
-                <div>
-                    <Button onClick={submit} text="Next" icon="arrow-forward" />
-                </div>
-            </Card>
+        <Card title={`Okay, ${name}`} icon="monkey-emoji">
+          <p className={styles.subHeading}>How’s this photo?</p>
+          <div className={styles.avatarWrapper}>
+            <img
+              className={styles.avatarImage}
+              alt="avatar"
+              src={image}
+            />
+          </div>
+          <div>
+            <input
+              onChange={captureImage}
+              className={styles.avatarInput}
+              id="avatarInput"
+              type="file"
+
+            />
+            <label className={styles.avatarLabel} htmlFor="avatarInput">
+              Choose a different photo
+            </label>
+          </div>
+          <div>
+            <Button onClick={submit} text="Next" icon="arrow-forward" />
+          </div>
+        </Card>
       </>
-     )
+    )
   )
 }
 
